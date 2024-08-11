24b82053e6b54be4ae65736d9046394f82815b14
Swadhin Kumar Nayak <nayak.swadhin25@gmail.com>
2024-08-11 22:59:28 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mysql from "mysql2";
+import cors from "cors";
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "9u9unLIKU^",
+  database: "test",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("Hello this a backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM test.books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully!!");
+  });
+});
+
+app.listen(8080, () => {
+  console.log("Listening to database1");
+});
